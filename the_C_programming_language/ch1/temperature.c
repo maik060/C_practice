201f267a2c99ce44c60bf59ee50f8b3b26a19757
maik060 <maik060@gmail.com>
2015-04-05 15:57:26 +0800

    better version

--- the_C_programming_language/ch1/temperature.c
+++ the_C_programming_language/ch1/temperature.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+#define FAHR_TO_CELSIUS(f) (5.0 / 9.0 * (f - 32.0))
+
 int main(int argc, char *argv[])
 {
-	int f;
-	for(f = 0; f <= 300; f += 20) {
-		printf("%d	%d\n", f, 5 * (f - 32) / 9);
+	float fahr;
+
+	for (fahr = LOWER; fahr <= UPPER; fahr += STEP) {
+		printf("%3.0f	%6.1f\n", fahr, FAHR_TO_CELSIUS(fahr));
 	}
+
 	return 0;
 }
